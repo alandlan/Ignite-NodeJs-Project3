c48116ceb0b471fc3ab41514f7c6b63ca508131e
Alan Martins <alanhorcrux@gmail.com>
2021-05-14 11:24:46 -0300

    Removido propriedade username

--- src/modules/accounts/entities/User.ts
+++ src/modules/accounts/entities/User.ts
@@ -3,35 +3,32 @@ import { v4 as uuidV4 } from "uuid";
 
 @Entity("users")
 class User {
-  @PrimaryColumn()
-  id: string;
-
-  @Column()
-  name: string;
-
-  @Column()
-  username: string;
-
-  @Column()
-  email: string;
-
-  @Column()
-  password: string;
-
-  @Column()
-  driver_license: string;
-
-  @Column()
-  isAdmin: boolean;
-
-  @CreateDateColumn()
-  created_at: Date;
-
   constructor() {
     if (!this.id) {
       this.id = uuidV4();
     }
   }
+
+  @PrimaryColumn()
+  id!: string;
+
+  @Column()
+  name!: string;
+
+  @Column()
+  email!: string;
+
+  @Column()
+  password!: string;
+
+  @Column()
+  driver_license!: string;
+
+  @Column()
+  isAdmin!: boolean;
+
+  @CreateDateColumn()
+  created_at!: Date;
 }
 
 export { User };
